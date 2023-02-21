32a9c46aba707c2794acaf6ffbce5022da5950d8
RosaD97 <r960595@gmail.com>
2023-02-21 14:52:40 +0100

    prezzo finale (da mettere solo prime due cifre)

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Calcolo del prezzo del biglietto del treno
 
 STEP1
 Chiedere all'utente il numero dei chilometri che vuole percorrere e salvare in una variabile
+//Calcolare il prezzo totale del biglietto sapendo che il costo ad ogni km percorso è di 0.21 €
 
 STEP2
 chiedere l'età dell'utente e salvare l'informazione in una variabile
---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Valori da chiedere all'utente
+const chilometri = prompt('Inserisci quanti chilometri vuoi percorrere');
+console.log(chilometri);
+
+const eta = prompt('Inserisci la tua età')
+console.log(eta);
+
+// Prezzo del biglietto al km
+const prezzoAlKm = 0.21;
+// Scontistiche
+const minorenni = 18;
+const over65 = 65;
+
+// Prezzo del biglietto finale
+let prezzoBiglietto = chilometri * prezzoAlKm;
+console.log(prezzoBiglietto);
+
+// Prezzo finale con sconti applicati
+if (eta <= minorenni)
+console.log(prezzoBiglietto - (prezzoBiglietto / 100 * 20));
+else if (eta >= over65)
+console.log(prezzoBiglietto - (prezzoBiglietto / 100 * 40));
